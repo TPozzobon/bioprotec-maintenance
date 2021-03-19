47b6c644a64d2539ca11c7bde4a8fefcecb19199
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-03-19 15:46:30 +0100

    create method edit and update

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -20,6 +20,17 @@ class EquipmentController < ApplicationController
     end
   end
 
+  def edit
+    @equipment = Equipment.find(params[:id])
+  end
+  
+  def update
+    @equipment = Equipment.find(params[:id])
+    @equipment.update(equipment_params)
+    redirect_to user_path(current_user)
+  end
+  
+
   private
 
   def equipment_params
