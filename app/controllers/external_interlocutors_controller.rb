21d1b34e791cf64d45a077c15aadc1157968b104
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-03-19 15:13:19 +0100

    add link_to external_interlocutor show view

--- app/controllers/external_interlocutors_controller.rb
+++ app/controllers/external_interlocutors_controller.rb
@@ -2,4 +2,9 @@ class ExternalInterlocutorsController < ApplicationController
   def index
     @external_interlocutors = ExternalInterlocutor.all
   end
+
+  def show
+    @external_interlocutor = ExternalInterlocutor.find(params[:id])
+  end
+  
 end
