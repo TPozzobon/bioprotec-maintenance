93cd13cddd9ea0dba4bf6976e4f703c981e5cdd9
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-03-19 15:18:02 +0100

    root the home page to equipment index

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -11,4 +11,18 @@ class EquipmentController < ApplicationController
     @equipment = Equipment.new
   end
   
+  def create
+    @equipment = Equipment.new(equipment_params)
+    if @equipment.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def equipment_params
+    params.require(:equipment).permit(:name, :identifiant, :location, :detail, :commissioning_date, :validity_qualification, :status)
+  end
 end
