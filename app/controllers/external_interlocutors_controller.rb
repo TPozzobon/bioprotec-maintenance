5dbdaf7a172bdacc553ca2f981112e7b05ccb0ff
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-03-22 10:43:20 +0100

    controller external_interlocutor

--- app/controllers/external_interlocutors_controller.rb
+++ app/controllers/external_interlocutors_controller.rb
@@ -6,5 +6,39 @@ class ExternalInterlocutorsController < ApplicationController
   def show
     @external_interlocutor = ExternalInterlocutor.find(params[:id])
   end
+
+  def new
+    @external_interlocutor = ExternalInterlocutor.new
+  end
+
+  def create
+    @external_interlocutor = ExternalInterlocutor.new(external_interlocutor_params)
+    if @external_interlocutor.save
+      redirect_to external_interlocutor_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @external_interlocutor = ExternalInterlocutor.find(params[:id])
+  end
   
+  def update
+    @external_interlocutor = ExternalInterlocutor.find(params[:id])
+    @external_interlocutor.update(external_interlocutor_params)
+    redirect_to external_interlocutor_path
+  end
+  
+  def destroy
+    @external_interlocutor = ExternalInterlocutor.find(params[:id])
+    @external_interlocutor.destroy
+    redirect_to external_interlocutor_path
+  end
+  
+  private
+
+  def external_interlocutor_params
+    params.require(:external_interlocutor).permit(:name, :company, :phone_number, :mobile_number, :email)
+  end
 end
