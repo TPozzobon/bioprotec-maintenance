06125677ecf6ec0b7b425a0444fd0316131e6ca1
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-03-25 08:41:56 +0100

    add external_interlocutor references to maintenance

---
+++ db/migrate/20210325074036_add_external_interlocutor_ref_to_maintenances.rb
@@ -0,0 +1,5 @@
+class AddExternalInterlocutorRefToMaintenances < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :maintenances, :external_interlocutor, null: false, foreign_key: true
+  end
+end
