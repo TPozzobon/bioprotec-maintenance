5759c220c80009090bf70c734215c0960b1d75f3
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-03-25 15:59:18 +0100

    add internal_interlocutor references to maintenance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_074036) do
+ActiveRecord::Schema.define(version: 2021_03_25_145844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,9 +75,11 @@ ActiveRecord::Schema.define(version: 2021_03_25_074036) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "equipment_id", null: false
-    t.bigint "external_interlocutor_id", null: false
+    t.bigint "external_interlocutor_id"
+    t.bigint "internal_interlocutor_id", null: false
     t.index ["equipment_id"], name: "index_maintenances_on_equipment_id"
     t.index ["external_interlocutor_id"], name: "index_maintenances_on_external_interlocutor_id"
+    t.index ["internal_interlocutor_id"], name: "index_maintenances_on_internal_interlocutor_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -95,4 +97,5 @@ ActiveRecord::Schema.define(version: 2021_03_25_074036) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "maintenances", "equipment"
   add_foreign_key "maintenances", "external_interlocutors"
+  add_foreign_key "maintenances", "internal_interlocutors"
 end
