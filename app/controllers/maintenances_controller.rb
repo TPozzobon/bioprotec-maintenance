d1054b0cac9e5af2536b05479eaf5be48adc5900
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-03-25 16:07:42 +0100

    add internal references to maintenance

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -10,15 +10,19 @@ class MaintenancesController < ApplicationController
   def new
     @external_interlocutors = ExternalInterlocutor.all
     @external_interlocutor = ExternalInterlocutor.where(external_interlocutor_id: params[:external_interlocutor_id])
+    @internal_interlocutors = InternalInterlocutor.all
+    @internal_interlocutor = InternalInterlocutor.where(internal_interlocutor_id: params[:internal_interlocutor_id])
     @equipment = Equipment.find(params[:equipment_id])
     @maintenance = Maintenance.new
   end
   
   def create
     @external_interlocutor = ExternalInterlocutor.find(params[:external_interlocutor_id])
+    @internal_interlocutor = InternalInterlocutor.find(params[:internal_interlocutor_id])
     @equipment = Equipment.find(params[:equipment_id])
     @maintenance = Maintenance.new(maintenance_params)
     @maintenance.external_interlocutor = @external_interlocutor
+    @maintenance.internal_interlocutor = @internal_interlocutor
     @maintenance.equipment = @equipment
     if @maintenance.save
       redirect_to equipment_path(@equipment)
