19e0782ca3ad2e4c6652e2b92e977849b2eb7054
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-04-12 16:05:00 +0200

    add start_date and end_date to maintenance model validate params

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -3,5 +3,5 @@ class Maintenance < ApplicationRecord
   belongs_to :external_interlocutor
   belongs_to :internal_interlocutor
 
-  validates :title, :date, :description, :status, presence: true
+  validates :title, :start_date, :end_date, :description, :status, presence: true
 end
