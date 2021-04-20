cff48a63f7d84a2e67a20fc01222d4d9d0beca44
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-04-20 11:48:43 +0200

    solve current_user problem

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -18,17 +18,20 @@ class MaintenancesController < ApplicationController
   def new
     @external_interlocutors = ExternalInterlocutor.all
     @external_interlocutor = ExternalInterlocutor.where(external_interlocutor_id: params[:external_interlocutor_id])
+    @users = User.all
+    @user = User.where(user_id: params[:user_id])
     @equipment = Equipment.find(params[:equipment_id])
     @maintenance = Maintenance.new
   end
   
   def create
     @external_interlocutor = ExternalInterlocutor.find(params[:external_interlocutor_id])
+    @user = User.find(params[:user_id])
     @equipment = Equipment.find(params[:equipment_id])
     @maintenance = Maintenance.new(maintenance_params)
     @maintenance.external_interlocutor = @external_interlocutor
+    @maintenance.user = @user
     @maintenance.equipment = @equipment
-    @maintenance.user = current_user
     if @maintenance.save
       redirect_to equipment_path(@equipment)
     else
