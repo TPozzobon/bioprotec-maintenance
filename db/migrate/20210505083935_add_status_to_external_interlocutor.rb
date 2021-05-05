130372e701c0230a1762b519711f67538b4e79fe
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-05-05 10:41:41 +0200

    add column status to external & users

---
+++ db/migrate/20210505083935_add_status_to_external_interlocutor.rb
@@ -0,0 +1,5 @@
+class AddStatusToExternalInterlocutor < ActiveRecord::Migration[6.0]
+  def change
+    add_column :external_interlocutors, :status, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_092940) do
+ActiveRecord::Schema.define(version: 2021_05_05_084047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_05_04_092940) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "detail"
+    t.string "status"
   end
 
   create_table "maintenances", force: :cascade do |t|
@@ -87,6 +88,7 @@ ActiveRecord::Schema.define(version: 2021_05_04_092940) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "visa"
     t.boolean "admin"
+    t.string "status"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
