d10e03eb942fa525a20559962664fd2d2907ce11
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-05-10 11:14:20 +0200

    change seed to have a inactive user and change the method index for user controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Equipment.destroy_all
 ExternalInterlocutor.destroy_all
 
 puts "Creating Users"
-User.create!(email: "user@gmail.com", password: "password", visa: "BBB", admin: false)
+User.create!(email: "user@gmail.com", password: "password", visa: "BBB", admin: false, status: "inactif")
 User.create!(email: "admin@gmail.com", password: "password", visa: "AAA", admin: true)
 
 puts "Creating Equipment"
