6251dc9425001fbd3127a5d3aa2aca8179b507b0
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-05-11 14:45:14 +0200

    all the migration is ok, change the model maintenances validations

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -3,5 +3,5 @@ class Maintenance < ApplicationRecord
   belongs_to :external_interlocutor
   belongs_to :user
 
-  validates :title, :start_date, :end_date, :description, :status, presence: true
+  validates :name, :start_time, :end_time, :description, :status, presence: true
 end
