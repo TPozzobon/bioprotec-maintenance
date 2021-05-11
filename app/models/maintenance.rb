48cfa484c16276a649e8900b2fd77c1737402c0e
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-05-11 16:38:16 +0200

    add the hours for the calendar && final design

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -4,4 +4,15 @@ class Maintenance < ApplicationRecord
   belongs_to :user
 
   validates :name, :start_time, :end_time, :description, :status, presence: true
+
+  default_scope -> { order(:start_time) }  # Our meetings will be ordered by their start_time by default
+  
+  def time
+    "#{start_time.strftime('%I:%M %p')} - #{end_time.strftime('%I:%M %p')}"
+  end
+  
+  def multi_days?
+    (end_time.to_date - start_time.to_date).to_i >= 1
+  end
+  
 end
