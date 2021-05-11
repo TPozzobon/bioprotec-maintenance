e674c77937e0f7d6db6804f9f542d2b5b5ca8e69
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-05-11 14:38:29 +0200

    change column start date by start time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_123514) do
+ActiveRecord::Schema.define(version: 2021_05_11_123629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2021_05_11_123514) do
 
   create_table "maintenances", force: :cascade do |t|
     t.string "name"
-    t.date "start_date"
+    t.date "start_time"
     t.text "description"
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
