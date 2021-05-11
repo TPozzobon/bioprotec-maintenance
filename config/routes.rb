ca817467a85c0c2dcee17d1d4de1ec8c4bb8e09f
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-05-11 15:42:58 +0200

    modifie root to page#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'equipment#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :equipment do
     resources :maintenances, only: [ :show, :new, :create ]
