462a35f7fb438a935e1101dba63d62fde707f297
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-05-20 11:16:11 +0200

    add criticity to equipment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_090308) do
+ActiveRecord::Schema.define(version: 2021_05_20_091407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_05_20_090308) do
     t.datetime "updated_at", precision: 6, null: false
     t.date "end_contract"
     t.string "recurrence"
+    t.boolean "criticity"
   end
 
   create_table "external_interlocutors", force: :cascade do |t|
