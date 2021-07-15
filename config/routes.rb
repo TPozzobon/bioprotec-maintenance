1ae0fe9758b20e87f4237fa32b8ba77ddb7c5502
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-07-15 11:39:07 +0200

    add commentary in all views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'maintenances#calendar'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :equipment do
-    resources :maintenances, only: [ :show, :new, :create ]
+    resources :maintenances, only: [ :new, :create ]
   end
   resources :maintenances, only: [ :index, :edit, :update, :destroy]
   resources :external_interlocutors
