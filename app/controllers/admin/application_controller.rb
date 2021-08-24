438d6df3d41589d723c1b26284b37c9603e4e13b
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-08-24 15:01:22 +0200

    delete authenticate user to module admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,7 +7,6 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     # before_action :authenticate_admin
-    before_action :authenticate_user!
 
     # def authenticate_admin
     #   # TODO Add authentication logic here.
