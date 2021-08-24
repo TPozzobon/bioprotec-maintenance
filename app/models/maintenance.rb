97eefef1df570ad7aa8aa83e9bd567ee58f90116
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-08-24 09:49:18 +0200

    delete a useless reference in external s model & delete description in validates in maintenance s model

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -3,7 +3,7 @@ class Maintenance < ApplicationRecord
   belongs_to :external_interlocutor
   belongs_to :user
 
-  validates :name, :start_time, :end_time, :description, :status, presence: true
+  validates :name, :start_time, :end_time, :status, presence: true
   
   def time
     "#{start_time.strftime('%I:%M %p')} - #{end_time.strftime('%I:%M %p')}"
